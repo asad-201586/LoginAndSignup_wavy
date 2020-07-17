b06c5051e6ea7a24d0a52cb1aa9c24d657471d68
asad-201586 <asadul201586@gmail.com>
2020-07-17 18:26:40 +0600

    SignupActivity added

--- app/src/main/java/com/asad/loginandsignup_wavy/SignupActivity.java
+++ app/src/main/java/com/asad/loginandsignup_wavy/SignupActivity.java
@@ -9,23 +9,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
-    private EditText eEmail,ePass;
-    private Button bLogin;
-    private TextView tForgotPass,tNewUser;
+    private EditText eName,ePhoneNo,eEmail,ePass;
+    private Button bSignUp;
+    private TextView tAlreadyhaveAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signup);
 
         initialize();
 
-        tNewUser.setOnClickListener(new View.OnClickListener() {
+        tAlreadyhaveAcc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,SignupActivity.class);
+                Intent intent = new Intent(SignupActivity.this,MainActivity.class);
                 startActivity(intent);
             }
         });
@@ -34,10 +34,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void initialize()
     {
+        eName = findViewById(R.id.name_editText_id);
+        ePhoneNo = findViewById(R.id.phone_no_editText_id);
         eEmail = findViewById(R.id.email_editText_id);
         ePass = findViewById(R.id.password_editText_id);
-        bLogin = findViewById(R.id.login_button_id);
-        tForgotPass = findViewById(R.id.forgot_pass_textView_id);
-        tNewUser = findViewById(R.id.new_user_textView_id);
+        bSignUp = findViewById(R.id.signup_button_id);
+        tAlreadyhaveAcc = findViewById(R.id.already_have_an_account);
     }
 }
